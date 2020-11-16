8bc578f78d6d235c471101efc00ed1a9ed375c01
Luka Vavetić <luka.vavetic@gmail.com>
2020-11-16 17:52:44 +0100

    firstClassAndHigherOrderFunc.js

---
+++ complete-javascript/functions/firstClassAndHigherOrderFunc.js
@@ -0,0 +1,12 @@
+// first-class functions
+// Javascript treats functions as first-class citizen
+// this means that functions are simply values
+// functions are just another "type" of objects
+// it can be stored in variables and properties
+// it can be passed as argument to other functions
+// it can be returned from functions 
+
+// higher-order functions
+// a function that receives another function as an argument, that returns a new function or both
+// this is only possible because of first-class functions
+// example of higher-order function = addEventListener func
